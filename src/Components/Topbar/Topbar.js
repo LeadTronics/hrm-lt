dfb43571bc27480c87dc39a6107efbd46c372eab
VidyaAG <vidyaagre789@gmail.com>
2022-04-06 19:25:52 +0530

    sidebar added

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -2,35 +2,49 @@ import React from 'react'
 import '../Components.css'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import profile from '../../assets/images/profile.jpg'
+import Offcanvas from 'react-bootstrap/Offcanvas'
 
 const Topbar = () => {
     return (
         <div className="Navbar-Style">
-                <Navbar expand={false}>
-                    <div className='d-flex'>
+            <Navbar expand={false}>
+                <div className='d-flex'>
                     <Navbar.Brand className="Dash-logo">HRM</Navbar.Brand>
-                    {/* <Nav.Link>Home</Nav.Link> */}
+                    <Navbar.Toggle className='me-3'>AM</Navbar.Toggle>
                     <form className="d-flex">
-                                        <input
-                                            type="search"
-                                            placeholder="Search"
-                                            className="me-2 form-control"
-                                            style={{ margin: "5px" }}
-                                            aria-label="Search"
-                                        />
-                                        <button style={{ margin: "5px" }} variant="outline-success">Search</button>
-                                    </form>
-                    </div>
-                    
+                        <input
+                            type="search"
+                            placeholder="Search"
+                            className="me-2 form-control"
+                            style={{ margin: "5px" }}
+                            aria-label="Search"
+                        />
+                        <button style={{ margin: "5px" }} variant="outline-success">Search</button>
+                    </form>
+                </div>
 
-                    <NavDropdown title={<img className="profile-top" src={profile} width="45px" alt="profile"></img>} id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Navbar>
+                <NavDropdown title={<img className="profile-top" src={profile} width="45px" alt="profile"></img>} id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="start"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel" className='text-center'>
+                        </Offcanvas.Title>
+
+                    </Offcanvas.Header>
+
+                    
+                </Navbar.Offcanvas>
+            </Navbar>
         </div>
     )
 }
