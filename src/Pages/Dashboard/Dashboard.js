00474b0978c062867afc71a31cc550de15ab873e
VidyaAG <vidyaagre789@gmail.com>
2022-04-06 15:47:01 +0530

    Topbar

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Topbar from '../../Components/Topbar/Topbar'
+import Header from '../../Components/Header/Header'
+
+const Dashboard = () => {
+    return (
+        <div className='dashboard'>
+            <div className='sidebar-text-content'>
+                <Topbar />
+                <div className='Page-Content'>
+                    <Header HeadingName="HR" MutedHeadName="  Dashboard" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Dashboard
