78684d765384b374d99bfb9efabb18f12a46a287
VidyaAG <vidyaagre789@gmail.com>
2022-04-06 19:11:21 +0530

    alignment fixed

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -5,11 +5,14 @@ import profile from '../../assets/images/profile.jpg'
 
 const Topbar = () => {
     return (
-        <div>
-            <div className='container-fluid'>
-                <Navbar>
+        <div className="Navbar-Style">
+                <Navbar expand={false}>
+                    <div className='d-flex'>
                     <Navbar.Brand className="Dash-logo">HRM</Navbar.Brand>
                     <Nav.Link>Home</Nav.Link>
+                    </div>
+                    
+
                     <NavDropdown title={<img className="profile-top" src={profile} width="45px" alt="profile"></img>} id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -18,7 +21,6 @@ const Topbar = () => {
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                 </Navbar>
-            </div>
         </div>
     )
 }
