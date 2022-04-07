14818ca715e45fd5d48064eb2e426828d6996311
VidyaAG <vidyaagre789@gmail.com>
2022-04-07 14:34:40 +0530

    api added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
 import Topbar from '../../Components/Topbar/Topbar'
 import Header from '../../Components/Header/Header'
+import {INTERVIEW_LIST} from '../../endpoint'
+import axios from 'axios'
 const Dashboard = () => {
 
+    const [Data, setData] = useState({})
+
+    useEffect(() => {
+        (async () => {
+            const user = JSON.parse(localStorage.getItem('token'));
+            const result = await axios.get(INTERVIEW_LIST, { headers: { Authorization: `Bearer ${user}` } });
+            if (result.status === 200) {
+                console.log(result.data)
+                setData(result.data)
+            }
+        })();
+    }, [])
     
     
 
