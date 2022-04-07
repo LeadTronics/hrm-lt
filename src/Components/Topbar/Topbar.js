b79368092df70b796e4033e3d2578b050647aeac
VidyaAG <vidyaagre789@gmail.com>
2022-04-07 11:22:29 +0530

    offcanvas removed

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import '../Components.css'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, NavDropdown } from 'react-bootstrap'
 import profile from '../../assets/images/profile.jpg'
-import { FaBars } from 'react-icons/fa'
-import { AiTwotoneHome, AiFillCalendar } from 'react-icons/ai'
-import { MdPeopleAlt } from 'react-icons/md'
-import { Link } from 'react-router-dom'
-import { RiTaskFill } from 'react-icons/ri'
-
 const Topbar = () => {
     return (
         <div className="Navbar-Style">
             <Navbar expand={false}>
                 <div className='d-flex'>
                     <Navbar.Brand className="Dash-logo">HRM</Navbar.Brand>
-                    <Link className='me-3' data-bs-toggle="offcanvas" to="#offcanvasExample" role="button"><FaBars /></Link>
                     <form className="d-flex">
                         <input
                             type="search"
