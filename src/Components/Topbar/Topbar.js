cb9bbc8c51c147fb76f1c2944bf3201e64e25e94
VidyaAG <vidyaagre789@gmail.com>
2022-04-07 10:41:30 +0530

    changes is elements

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -54,21 +54,7 @@ const Topbar = () => {
                             </div>
 
                         </div>
-                        <Nav style={{ padding: "10px 35px" }}>
-                            <Nav.Link href="/dashboard"><AiTwotoneHome style={{ color: "#aab3cc", marginRight: "10px" }} />Dashboard</Nav.Link>
-                            <NavDropdown title={<span><MdPeopleAlt style={{ color: "#aab3cc", marginRight: "10px" }} />Employee</span>} id="offcanvasNavbarDropdown">
-                                <NavDropdown.Item><Link to="/allemployee">Employee List</Link></NavDropdown.Item>
-                                <NavDropdown.Item><Link to="/addemployee">Add Employee Details</Link></NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown title={<span><RiTaskFill style={{ color: "#aab3cc", marginRight: "10px" }} />Interview</span>} id="offcanvasNavbarDropdown">
-                                <NavDropdown.Item><Link to="/interviewschedule">Interview Schedule</Link></NavDropdown.Item>
-                                <NavDropdown.Item><Link to="/interviewtabledata">Interview Schedule List</Link></NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown title={<span><AiFillCalendar style={{ color: "#aab3cc", marginRight: "10px" }} />Events</span>} id="offcanvasNavbarDropdown">
-                                <NavDropdown.Item><Link to="/add-event">Add Event</Link></NavDropdown.Item>
-                                <NavDropdown.Item><Link to="/all-events">All Events</Link></NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
+                        
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Navbar>
