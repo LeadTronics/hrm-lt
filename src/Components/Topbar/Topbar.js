077b6edcf542aad0e8cba2b3fad3418191bd77cf
VidyaAG <vidyaagre789@gmail.com>
2022-04-07 11:03:25 +0530

    offcanvas removed

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import '../Components.css'
-import { Navbar, Nav, NavDropdown, OffcanvasBody } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import profile from '../../assets/images/profile.jpg'
-import Offcanvas from 'react-bootstrap/Offcanvas'
 import { FaBars } from 'react-icons/fa'
-import {AiTwotoneHome, AiFillCalendar}  from 'react-icons/ai'
-import {MdPeopleAlt} from 'react-icons/md'
+import { AiTwotoneHome, AiFillCalendar } from 'react-icons/ai'
+import { MdPeopleAlt } from 'react-icons/md'
 import { Link } from 'react-router-dom'
-import {RiTaskFill} from 'react-icons/ri'
+import { RiTaskFill } from 'react-icons/ri'
 
 const Topbar = () => {
     return (
@@ -15,7 +14,7 @@ const Topbar = () => {
             <Navbar expand={false}>
                 <div className='d-flex'>
                     <Navbar.Brand className="Dash-logo">HRM</Navbar.Brand>
-                    <Navbar.Toggle className='me-3'><FaBars /></Navbar.Toggle>
+                    <Link className='me-3' data-bs-toggle="offcanvas" to="#offcanvasExample" role="button"><FaBars /></Link>
                     <form className="d-flex">
                         <input
                             type="search"
@@ -36,21 +35,36 @@ const Topbar = () => {
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
 
-                <Navbar.Offcanvas
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel"
-                    placement="start">
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel" className='text-center'>
-                        </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <>
-                        <Link to="/">dashboard</Link>
-                        </>
-                        
-                    </Offcanvas.Body>
-                </Navbar.Offcanvas>
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasExampleLabel">HRM</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div className='py-3 border-Bgray text-center'>
+                            <div className='py-2'>
+                                <h5 className='text-white mb-0'>username</h5>
+                                <div className='text-muted'>Software Developer</div>
+                            </div>
+                        </div>
+                        <Nav style={{padding:"10px 35px"}}>
+                                            <Nav.Link href="/dashboard"><AiTwotoneHome style={{ color: "#aab3cc", marginRight: "10px" }} />Dashboard</Nav.Link>
+                                            <NavDropdown title={<span><MdPeopleAlt style={{ color: "#aab3cc", marginRight: "10px" }} />Employee</span>} id="offcanvasNavbarDropdown">
+                                                <NavDropdown.Item><Link to="/allemployee">Employee List</Link></NavDropdown.Item>
+                                                <NavDropdown.Item><Link to="/addemployee">Add Employee Details</Link></NavDropdown.Item>
+                                            </NavDropdown>
+                                            <NavDropdown title={<span><RiTaskFill style={{ color: "#aab3cc", marginRight: "10px" }} />Interview</span>} id="offcanvasNavbarDropdown">
+                                                <NavDropdown.Item><Link to="/interviewschedule">Interview Schedule</Link></NavDropdown.Item>
+                                                <NavDropdown.Item><Link to="/interviewtabledata">Interview Schedule List</Link></NavDropdown.Item>
+                                            </NavDropdown>
+                                            <NavDropdown title={<span><AiFillCalendar style={{ color: "#aab3cc", marginRight: "10px" }} />Events</span>} id="offcanvasNavbarDropdown">
+                                                <NavDropdown.Item><Link to="/add-event">Add Event</Link></NavDropdown.Item>
+                                                <NavDropdown.Item><Link to="/all-events">All Events</Link></NavDropdown.Item>
+                                            </NavDropdown>
+                                        </Nav>
+                    </div>
+                </div>
             </Navbar>
         </div>
     )
